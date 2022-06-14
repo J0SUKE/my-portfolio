f189178603f2bacf7a6dfc918e3f8111822e3930
Jean <chakib3500@gmail.com>
2022-06-14 14:56:40 +0200

    contact page

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -5,6 +5,7 @@ import { useEffect,useRef } from 'react';
 import Link from 'next/link';
 import Projects from '../Projects/Projects';
 import { throttle } from 'lodash';
+import Typewriter from 'typewriter-effect';
 
 export default function Home() {
     
@@ -115,7 +116,7 @@ export default function Home() {
                   <img src="/images/presentation.jpg" alt="" />
               </div>
               <div className={styles.presentation__content__text} ref={presentationText}>
-                  I am a 21 years old <strong>computer science student</strong> based in <strong>Lyon</strong>. I started learning programmig on my own in <strong>2020</strong> , started with Python then C++ then i learned Linux and basic web architecture ,and finally got into web programming , i started with HTML , CSS javascript and did some PHP , after that i chose to master <strong>Javascript</strong> so i passed the JavaScript Algorithms and Data Structures <strong>certification</strong> on freecodecamp.org that you can find <a href={'https://www.freecodecamp.org/certification/fcc58c90e68-b03b-4494-8ab1-13af0fb0cfaf/javascript-algorithms-and-data-structures'} target='_blank' rel="noreferrer">here</a>. I built some <a href="#projects">projects</a> with Vanilla Javascript and then as my projects became more complex i switched to <strong>React</strong> and fell in love with it 😍.
+                  I am a 21 years old <strong>freelance web developer</strong> based in <strong>Lyon</strong>. I started learning programmig on my own in <strong>2020</strong> , started with Python then C++ then i learned Linux and basic web architecture ,and finally got into web programming , i started with HTML , CSS javascript and did some PHP , after that i chose to master <strong>Javascript</strong> so i passed the JavaScript Algorithms and Data Structures <strong>certification</strong> on freecodecamp.org that you can find <a href={'https://www.freecodecamp.org/certification/fcc58c90e68-b03b-4494-8ab1-13af0fb0cfaf/javascript-algorithms-and-data-structures'} target='_blank' rel="noreferrer">here</a>. I built some <a href="#projects">projects</a> with Vanilla Javascript and then as my projects became more complex i switched to <strong>React</strong> and fell in love with it 😍.
               </div>
             </div>
               </section>
@@ -123,7 +124,7 @@ export default function Home() {
                 <div className={styles.skills__content_container} id="skills">
                   <div className={styles.skills__content}>
                   <h1 ref={skillsTitls}>Web Technologies & Soft skills</h1>
-                  <p ref={p1}>As someone who loves discovering new technologies , I am not afraid of learning new tools on the go ,i think it is very important to use the right tool for the right task .I am also a fast learner so i can adapt to any stack without any language or framework bareer.</p>
+                  <p ref={p1}>As someone who loves discovering new technologies , I am not afraid of learning new tools on the go as i think it is very important to use the right tool for the right task .I am also a fast learner so i can adapt to any stack without any language or framework bareer.</p>
                   <p ref={p2}>Here are some tech-tools and libraries i am good at </p>
 
                   <section>
@@ -180,7 +181,37 @@ export default function Home() {
               </section>
               <Projects/>
               <div className={styles.contact}>
-                <h1>Contact me</h1>
+                <div className={styles.contact__content}>
+                  <div className={styles.contact__content__left}>
+                    <img src="/images/programming.svg" alt="" />
+                  </div>
+                  <div className={styles.contact__content__right}>
+                    <h1>Let&apos;s make something great together.</h1>
+                    <p>Whether it&apos;s a new adventure or existing project.</p>
+                    <ul>
+                      <li>
+                        <a href="https://twitter.com/Jean_M_____I" target="__blank" rel="noreferrer">
+                          <svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 24 24"><path fill="#000000" d="M22,5.8a8.49,8.49,0,0,1-2.36.64,4.13,4.13,0,0,0,1.81-2.27,8.21,8.21,0,0,1-2.61,1,4.1,4.1,0,0,0-7,3.74A11.64,11.64,0,0,1,3.39,4.62a4.16,4.16,0,0,0-.55,2.07A4.09,4.09,0,0,0,4.66,10.1,4.05,4.05,0,0,1,2.8,9.59v.05a4.1,4.1,0,0,0,3.3,4A3.93,3.93,0,0,1,5,13.81a4.9,4.9,0,0,1-.77-.07,4.11,4.11,0,0,0,3.83,2.84A8.22,8.22,0,0,1,3,18.34a7.93,7.93,0,0,1-1-.06,11.57,11.57,0,0,0,6.29,1.85A11.59,11.59,0,0,0,20,8.45c0-.17,0-.35,0-.53A8.43,8.43,0,0,0,22,5.8Z"/></svg>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="mailto:jeanmazouni@gmail.com" target="__blank" rel="noreferrer">
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#000000" d="M19,4H5A3,3,0,0,0,2,7V17a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V7A3,3,0,0,0,19,4ZM5,6H19a1,1,0,0,1,1,1l-8,4.88L4,7A1,1,0,0,1,5,6ZM20,17a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V9.28l7.48,4.57a1,1,0,0,0,1,0L20,9.28Z"/></svg>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="" target="__blank" rel="noreferrer">
+                          <svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 24 24"><path fill="#000000" d="M20.47,2H3.53A1.45,1.45,0,0,0,2.06,3.43V20.57A1.45,1.45,0,0,0,3.53,22H20.47a1.45,1.45,0,0,0,1.47-1.43V3.43A1.45,1.45,0,0,0,20.47,2ZM8.09,18.74h-3v-9h3ZM6.59,8.48h0a1.56,1.56,0,1,1,0-3.12,1.57,1.57,0,1,1,0,3.12ZM18.91,18.74h-3V13.91c0-1.21-.43-2-1.52-2A1.65,1.65,0,0,0,12.85,13a2,2,0,0,0-.1.73v5h-3s0-8.18,0-9h3V11A3,3,0,0,1,15.46,9.5c2,0,3.45,1.29,3.45,4.06Z"/></svg>
+                        </a>                        
+                      </li>
+                    </ul>
+                  </div>
+                  
+                  
+                </div>
+                <div className={styles.copyright}>
+                    <p>© 2022 Jean Mazouni. All Rights Reserved.</p>
+                  </div>
               </div>
           </section>  
       </main>
