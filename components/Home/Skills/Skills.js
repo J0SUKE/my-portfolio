132b8fbd11862cd6d3d3b09347cd4da4e1cf2baa
Jean <chakib3500@gmail.com>
2022-06-17 11:05:23 +0200

    intersection observer

--- components/Home/Skills/Skills.js
+++ components/Home/Skills/Skills.js
@@ -9,7 +9,7 @@ import SVGwave from '../../SVG/SVGwave';
 
 export default function Skills() {
   
-    const [line1,line2,line3,skillsTitls,p1,p2] = useObserveSkills(styles.active);
+    const [line1,line2,line3,skillsTitls,p] = useObserveSkills(styles.active);
     const lines = [line1,line2,line3]; // chaque line correspond a une categorie (frontend , backend , soft)
     
     const {skills} = useContext(AppContext);
@@ -18,29 +18,30 @@ export default function Skills() {
     <section className={styles.skills}>
         <div className={styles.skills__content_container} id="skills">
             <div className={styles.skills__content}>
-            <h1 ref={skillsTitls}>Web Technologies & Soft skills</h1>
-            <p ref={p1}>As someone who loves discovering new technologies, I am not afraid of learning new tools on the go as I think it is very important to use the right tool for the right task. I am also a fast learner, so I can adapt to any stack without any language or framework barriers.</p>
-            <p ref={p2}>Here are some tech-tools and libraries I am good at</p>
+              <h1 ref={skillsTitls}>Web Technologies & Soft skills</h1>
+              <div ref={p}>
+                <p>As someone who loves discovering new technologies, I am not afraid of learning new tools on the go as I think it is very important to use the right tool for the right task. I am also a fast learner, so I can adapt to any stack without any language or framework barriers.<br/><br/>
+                Here are some tech-tools and libraries I am good at</p>
+              </div>
 
-            <section>
-            {
-            skills.map((item,index)=>{
-                const {attributes:{skills:{data}}} = item;
-                return (
-                <ul ref={lines[index]} key={item.id}>
+              <section>
+              {
+                skills.map((item,index)=>{
+                  const {attributes:{skills:{data}}} = item;
+                  return (
+                  <ul ref={lines[index]} key={item.id}>
                     {
-                    data.map((item)=>{
-                        const {attributes} = item;
-                        return (
-                        <li key={item.id}>
-                            <img src={getStrapiMedia(attributes.image)} alt="" />
-                            {attributes.name}
-                            
-                        </li>  
+                      data.map((item)=>{
+                          const {attributes} = item;
+                          return (
+                          <li key={item.id}>
+                              <img src={getStrapiMedia(attributes.image)} alt="" />
+                              {attributes.name}                            
+                          </li>  
                         )
                     })
                     }
-                </ul>
+                  </ul>
                 )
             })
             }
@@ -61,11 +62,10 @@ function useObserveSkills(activeClass) {
     const line2 = useRef();
     const line3 = useRef();
     const skillsTitls = useRef();
-    const p1 = useRef();
-    const p2 = useRef();
+    const p = useRef();
   
   
-    const entries = [line1,line2,line3,skillsTitls,p1,p2];
+    const entries = [line1,line2,line3,skillsTitls,p];
   
     useEffect(()=>{
         let observer = new IntersectionObserver((entries)=>{
@@ -76,7 +76,7 @@ function useObserveSkills(activeClass) {
           });  
             
         },{
-          threshold:0.8
+          threshold:0.5
         })
   
         if (line1 && line2 && line3) {
@@ -87,8 +87,8 @@ function useObserveSkills(activeClass) {
         
         
         
-    },[line1,line2,line3,skillsTitls,p1,p2])
+    },[line1,line2,line3,skillsTitls,p])
   
   
-    return [line1,line2,line3,skillsTitls,p1,p2];
+    return [line1,line2,line3,skillsTitls,p];
   }
